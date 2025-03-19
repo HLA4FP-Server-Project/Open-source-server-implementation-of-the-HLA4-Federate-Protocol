21efe865138572f5ab638deb71f2aa92ad5fe467
Tgb03 <69427849+Tgb03@users.noreply.github.com>
2025-03-19 16:43:49 +0100

    Moved server to its own module and made it more generic. Now it can have any connection initializer and client handler.

--- src/main/java/com/rug/tno/server/Server.java
+++ src/main/java/com/rug/tno/server/Server.java
@@ -1,41 +1,37 @@
-package com.rug.tno;
+package com.rug.tno.server;
 
-import java.net.InetSocketAddress;
-
+import com.rug.tno.connection.HandlerFactory;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
 
 public class Server {
 
     private Integer port;
+    private HandlerFactory channelInitializer;
 
-    public Server(Integer port){
+    public Server(Integer port, HandlerFactory channelInitializer){
         this.port = port;
+        this.channelInitializer = channelInitializer;
     }
 
     public void run() throws Exception{
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try{
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-            .channel(NioServerSocketChannel.class)
-            .childHandler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    System.out.println("Incoming connection from " + ch.remoteAddress());
-                    ch.pipeline().addLast(new ServerHandler());
-                }
-            })
-            .option(ChannelOption.SO_BACKLOG, 128)
-            .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ServerBootstrap b = new ServerBootstrap()
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(
+                        channelInitializer.createHandler()
+                )
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
 
             ChannelFuture f = b.bind(new InetSocketAddress("0.0.0.0", port)).sync();
 
