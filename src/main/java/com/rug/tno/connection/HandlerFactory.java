808677d51152a01b0b3aad7ef0c8882a22f35fe6
Tgb03 <69427849+Tgb03@users.noreply.github.com>
2025-03-19 16:40:19 +0100

    Added the basic stuff for the connection module.

---
+++ src/main/java/com/rug/tno/connection/HandlerFactory.java
@@ -0,0 +1,22 @@
+package com.rug.tno.connection;
+
+import io.netty.channel.ChannelHandler;
+
+/**
+ * Factory to produce channel handlers
+ *
+ * Each class in the connection module needs to implement this,
+ * so it can then be passed down to the server, so it can
+ * create the initial connection according to the desired
+ * parameters
+ */
+public interface HandlerFactory {
+
+    /**
+     * create a single handler to be passed to the server
+     * @return handler which handles the connection once
+     *      instantiated
+     */
+    ChannelHandler createHandler();
+
+}
---
+++ src/main/java/com/rug/tno/connection/TcpConnection.java
@@ -0,0 +1,33 @@
+package com.rug.tno.connection;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * TcpConnection creates a ChannelHandler that can then handle the client
+ */
+public class TcpConnection implements HandlerFactory {
+
+    private HandlerFactory handlerFactory;
+
+    /**
+     * Basic constructor. Takes a single handler factory that constructs client handlers
+     * You can also give it Handler::new as reference if Handler extends ChannelHandlerAdapter
+     * @param handlerFactory client handler factory
+     */
+    public TcpConnection(HandlerFactory handlerFactory) {
+        this.handlerFactory = handlerFactory;
+    }
+
+    @Override
+    public ChannelHandler createHandler() {
+        return new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) {
+                System.out.println("Incoming connection from " + socketChannel.remoteAddress());
+                socketChannel.pipeline().addLast(handlerFactory.createHandler());
+            }
+        };
+    }
+}
