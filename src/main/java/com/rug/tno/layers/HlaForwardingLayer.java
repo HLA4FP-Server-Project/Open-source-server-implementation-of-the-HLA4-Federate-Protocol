0a031b107b649cde179577ff48d9bf647df8ab5c
Kieran McGeehan <167707711+KieranMcGeehan@users.noreply.github.com>
2025-03-27 20:38:01 +0100

    Implement more hla messages

--- src/main/java/com/rug/tno/layers/HlaForwardingLayer.java
+++ src/main/java/com/rug/tno/layers/HlaForwardingLayer.java
@@ -1,10 +1,15 @@
 package com.rug.tno.layers;
 
+import com.google.protobuf.ByteString;
 import com.rug.tno.fpdata.FpMessage;
 import com.rug.tno.fpdata.HlaCallRequest;
 import com.rug.tno.fpdata.HlaCallResponse;
 import com.rug.tno.url.OneTimeUseUrl;
 import hla.rti1516_2024.fedpro.*;
+import hla.rti1516_2024.fedpro.AttributeHandle;
+import hla.rti1516_2024.fedpro.InteractionClassHandle;
+import hla.rti1516_2024.fedpro.ObjectClassHandle;
+import hla.rti1516_2024.fedpro.ParameterHandle;
 import hla.rti1516_202X.*;
 import hla.rti1516_202X.AdditionalSettingsResultCode;
 import hla.rti1516_202X.CallbackModel;
@@ -15,8 +20,10 @@ import hla.rti1516_202X.exceptions.RTIexception;
 import hla.rti1516_202X.exceptions.RTIinternalError;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.apache.commons.lang3.function.TriConsumer;
 
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class HlaForwardingLayer extends ChannelInboundHandlerAdapter {
     private final String serverAddress;
@@ -108,8 +115,143 @@ public class HlaForwardingLayer extends ChannelInboundHandlerAdapter {
                         )
                         .build();
             }
+            case GETINTERACTIONCLASSHANDLEREQUEST -> {
+                var getRequest = request.getGetInteractionClassHandleRequest();
+                var response = this.rtiAmbassador.getInteractionClassHandle(getRequest.getInteractionClassName());
+                var responseBytes = encodeClassHandle(response);
+                return CallResponse.newBuilder()
+                        .setGetInteractionClassHandleResponse(
+                                GetInteractionClassHandleResponse.newBuilder()
+                                        .setResult(
+                                                InteractionClassHandle.newBuilder()
+                                                        .setData(responseBytes)
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+            }
+            case GETPARAMETERHANDLEREQUEST -> {
+                var getRequest = request.getGetParameterHandleRequest();
+                var response = this.rtiAmbassador.getParameterHandle(decodeClassHandle(getRequest.getInteractionClass()), getRequest.getParameterName());
+                var responseBytes = encodeParamHandle(response);
+                return CallResponse.newBuilder()
+                        .setGetParameterHandleResponse(
+                                GetParameterHandleResponse.newBuilder()
+                                        .setResult(
+                                                ParameterHandle.newBuilder()
+                                                        .setData(responseBytes)
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+            }
+            case GETOBJECTCLASSHANDLEREQUEST -> {
+                var getRequest = request.getGetObjectClassHandleRequest();
+                var response = this.rtiAmbassador.getObjectClassHandle(getRequest.getObjectClassName());
+                var responseBytes = encodeObjHandle(response);
+                return CallResponse.newBuilder()
+                        .setGetObjectClassHandleResponse(
+                                GetObjectClassHandleResponse.newBuilder()
+                                        .setResult(
+                                                ObjectClassHandle.newBuilder()
+                                                        .setData(responseBytes)
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+            }
+            case GETATTRIBUTEHANDLEREQUEST -> {
+                var getRequest = request.getGetAttributeHandleRequest();
+                var response = this.rtiAmbassador.getAttributeHandle(decodeObjHandle(getRequest.getObjectClass()), getRequest.getAttributeName());
+                var responseBytes = encodeAttrHandle(response);
+                return CallResponse.newBuilder()
+                        .setGetAttributeHandleResponse(
+                                GetAttributeHandleResponse.newBuilder()
+                                        .setResult(
+                                                AttributeHandle.newBuilder()
+                                                        .setData(responseBytes)
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+            }
+            case SUBSCRIBEINTERACTIONCLASSREQUEST -> {
+                var subRequest = request.getSubscribeInteractionClassRequest();
+                this.rtiAmbassador.subscribeInteractionClass(decodeClassHandle(subRequest.getInteractionClass()));
+                return CallResponse.newBuilder()
+                        .setSubscribeInteractionClassResponse(
+                                SubscribeInteractionClassResponse.newBuilder().build()
+                        )
+                        .build();
+            }
+            case PUBLISHINTERACTIONCLASSREQUEST -> {
+                var pubRequest = request.getPublishInteractionClassRequest();
+                this.rtiAmbassador.subscribeInteractionClass(decodeClassHandle(pubRequest.getInteractionClass()));
+                return CallResponse.newBuilder()
+                        .setPublishInteractionClassResponse(
+                                PublishInteractionClassResponse.newBuilder().build()
+                        )
+                        .build();
+            }
             default -> throw new IllegalStateException("Unknown call request " + request.getCallRequestCase());
         }
+    }
+
+    private ByteString encodeAttrHandle(hla.rti1516_202X.AttributeHandle handle) {
+        return encodeHandle(
+                handle,
+                hla.rti1516_202X.AttributeHandle::encodedLength,
+                hla.rti1516_202X.AttributeHandle::encode
+        );
+    }
+
+    private ByteString encodeObjHandle(hla.rti1516_202X.ObjectClassHandle handle) {
+        return encodeHandle(
+                handle,
+                hla.rti1516_202X.ObjectClassHandle::encodedLength,
+                hla.rti1516_202X.ObjectClassHandle::encode
+        );
+    }
+
+    private ByteString encodeParamHandle(hla.rti1516_202X.ParameterHandle handle) {
+        return encodeHandle(
+                handle,
+                hla.rti1516_202X.ParameterHandle::encodedLength,
+                hla.rti1516_202X.ParameterHandle::encode
+        );
+    }
+
+    private ByteString encodeClassHandle(hla.rti1516_202X.InteractionClassHandle handle) {
+        return encodeHandle(
+                handle,
+                hla.rti1516_202X.InteractionClassHandle::encodedLength,
+                hla.rti1516_202X.InteractionClassHandle::encode
+        );
+    }
+
+    private <T> ByteString encodeHandle(T obj, Function<T, Integer> encodedLength, TriConsumer<T, byte[], Integer> encode) {
+        var responseBytes = new byte[encodedLength.apply(obj)];
+        encode.accept(obj, responseBytes, 0);
+        return ByteString.copyFrom(responseBytes);
+    }
+
+    private hla.rti1516_202X.ObjectClassHandle decodeObjHandle(ObjectClassHandle handle) throws RTIexception {
+        var factory = this.rtiAmbassador.getObjectClassHandleFactory();
+        return factory.decode(handle.toByteArray(), 0);
+    }
+
+    private hla.rti1516_202X.ParameterHandle decodeParamHandle(ParameterHandle handle) throws RTIexception {
+        var factory = this.rtiAmbassador.getParameterHandleFactory();
+        return factory.decode(handle.toByteArray(), 0);
+    }
+
+    private hla.rti1516_202X.InteractionClassHandle decodeClassHandle(InteractionClassHandle handle) throws RTIexception {
+        var factory = this.rtiAmbassador.getInteractionClassHandleFactory();
+        return factory.decode(handle.toByteArray(), 0);
     }
 
     private static String[] fomModulesToUrls(FomModuleSet set) {
